4ee4938dddff24c954cc5a9d2a41e0f11661e37f
giovanni <giovanni@MacBook-Air-6.local>
2026-04-15 20:29:41 +0200

    Anchor bottom nav content flush

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -37,11 +37,7 @@ export default function BottomNav() {
   return (
     <>
       <div
-        className="fixed left-0 right-0 w-full translate-y-0 will-change-transform border-t border-black/10 bg-white shadow-[0_-10px_24px_rgba(0,0,0,0.05)] flex items-start pt-0 pb-0 z-50"
-        style={{
-          bottom: "calc(env(safe-area-inset-bottom, 0px) * -1)",
-          height: "calc(54px + env(safe-area-inset-bottom, 0px))",
-        }}
+        className="fixed bottom-0 left-0 right-0 w-full h-[54px] translate-y-0 will-change-transform border-t border-black/10 bg-white shadow-[0_-10px_24px_rgba(0,0,0,0.05)] flex items-end pt-0 pb-0 z-50"
       >
         {navItems.map((item) => {
           const active = isActive(item.href);

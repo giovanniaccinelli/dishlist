d7a9e73deca112eca83074544c1c38bc98e6ac9d
giovanni <giovanni@MacBook-Air-7.local>
2026-05-18 22:44:59 +0200

    Adjust calendar preview binding holes

--- app/profile/page.js
+++ app/profile/page.js
@@ -2284,11 +2284,11 @@ export default function Profile() {
                 >
                   <div className="relative flex h-full flex-col">
                     <div className={`relative h-6 border-b ${darkMode ? "border-white/8 bg-white/10" : "border-black/8 bg-[#ECE7DA]"}`}>
-                      <div className="absolute left-0 right-0 top-1/2 flex -translate-y-1/2 justify-center gap-4">
-                        {[0, 1, 2].map((item) => (
+                      <div className="absolute left-0 right-0 top-1/2 flex -translate-y-1/2 justify-center gap-8">
+                        {[0, 1].map((item) => (
                           <span
                             key={item}
-                            className={`h-2 w-2 rounded-full shadow-[inset_0_1px_2px_rgba(0,0,0,0.24)] ${
+                            className={`h-3 w-3 rounded-full shadow-[inset_0_1px_2px_rgba(0,0,0,0.28)] ${
                               darkMode ? "bg-[#181818]" : "bg-[#FBFAF6]"
                             }`}
                           />
